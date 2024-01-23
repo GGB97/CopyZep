ee54ed9fc1bf7a99ef7af7983f301745de7e9a2f
GGB <rlqja1018@gmail.com>
2024-01-23 09:59:21 +0900

    UI 개선
    게임도중 좌측 상단에 현재 시간 표시

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Transform playerTransform;
+    public Transform playerTransform;
     Vector3 cameraPosition;
 
     private void Awake()
--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,23 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    CameraController mainCamera;
+    [SerializeField] TMP_Text timeText;
 
     GameObject playerPrefab;
 
     private void Awake()
     {
         Instance = this;
+        mainCamera = Camera.main.GetComponent<CameraController>();
 
         int idx = PlayerPrefs.GetInt("PlayerCharIndex");
         playerPrefab = StartManager.Instance.charPrefabList[idx]; // 이렇게 할거면 그냥 prefab을 가져오면 되는거 아닌가?
     }
 
+    private void Update()
+    {
+        UpdateTime();
+    }
+
     private void Start()
     {
         GameObject player = Instantiate(playerPrefab);
+        mainCamera.playerTransform = player.transform;
+    }
+
+    void UpdateTime()
+    {
+        // 현재 시간을 가져와서 텍스트로 변환
+        DateTime currentTime = DateTime.Now;
+        string timeString = currentTime.ToString("HH:mm");
+
+        // 텍스트 업데이트
+        timeText.text = timeString;
     }
 }
